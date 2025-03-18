0bc2491661b81e6d784a24489f251295f5860045
Charles-Antoine <charto.bta@gmail.com>
2025-03-18 11:16:59 +0100

    test

---
+++ src/test/java/com/cab/Collection2DTest.java
@@ -0,0 +1,126 @@
+package com.cab;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Collection2DTest {
+
+    public static final int NB_ELEMENTS_TO_ADD = 100;
+    public static final int NB_ROWS = 100;
+    public static final int NB_COLUMNS = 200;
+
+    private static Collection2D<Collection2DElementTest> collection2D;
+    private static List<Collection2DElementTest> elements;
+
+    @BeforeEach
+    void beforeEach() {
+        Collection2DTest.collection2D = new Collection2D<>();
+        Collection2DTest.elements = new ArrayList<>();
+
+        for (int numElements = 0; numElements < NB_ELEMENTS_TO_ADD; numElements++) {
+            for (int row = 0; row < NB_ROWS; row++) {
+                for (int column = 0; column < NB_COLUMNS; column++) {
+                    Collection2DElementTest element = new Collection2DElementTest();
+                    element.setPosition(new Point(column, row));
+                    collection2D.add(element);
+                    elements.add(element);
+                }
+            }
+        }
+    }
+
+    @Test
+    void addAndGet() {
+        for (Collection2DElementTest elementTest : Collection2DTest.elements) {
+            List<Collection2DElementTest> actualElementsAtPosition = collection2D.get(elementTest.getPosition());
+
+            assertNotNull(actualElementsAtPosition, "Element list at position " + elementTest.position + " is null");
+            assertEquals(Collection2DTest.NB_ELEMENTS_TO_ADD, actualElementsAtPosition.size(), "Element list at position " + elementTest.position + " has more than one element");
+            assertTrue(actualElementsAtPosition.contains(elementTest), "Element list at position " + elementTest.position + " does not contain the element");
+        }
+
+        Collection2DElementTest newElement = new Collection2DElementTest();
+        newElement.setPosition(new Point(-100, -10));
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> Collection2DTest.collection2D.add(newElement), "Element with negative position has been added");
+        newElement.setPosition(new Point(100, -10));
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> Collection2DTest.collection2D.add(newElement), "Element with negative position has been added");
+        newElement.setPosition(new Point(-100, 10));
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> Collection2DTest.collection2D.add(newElement), "Element with negative position has been added");
+        assertThrows(IllegalArgumentException.class, () -> Collection2DTest.collection2D.add(null));
+    }
+
+    @Test
+    void remove() {
+        for (Collection2DElementTest elementTest : Collection2DTest.elements) {
+            collection2D.remove(elementTest);
+
+            List<Collection2DElementTest> actualElementsAtPosition = collection2D.get(elementTest.getPosition());
+
+            assertNotNull(actualElementsAtPosition, "Element list at position " + elementTest.position + " is null");
+            assertFalse(actualElementsAtPosition.contains(elementTest), "Element list at position " + elementTest.position + " still contains the element");
+            assertThrows(IllegalArgumentException.class, () -> Collection2DTest.collection2D.remove(null), "Element with negative position has been added");
+        }
+    }
+
+    @Test
+    void contains() {
+        for (Collection2DElementTest elementTest : Collection2DTest.elements) {
+            assertTrue(collection2D.contains(elementTest), "Element " + elementTest + " not found in collection");
+        }
+        assertThrows(IllegalArgumentException.class, () -> Collection2DTest.collection2D.contains(null), "Element with negative position has been added");
+    }
+
+    @Test
+    void toList() {
+        assertNotNull(Collection2DTest.collection2D.toList(), "Collection2D toList is null");
+        assertEquals(Collection2DTest.elements.size(), Collection2DTest.collection2D.toList().size(), "Collection2D toList size is not the same as the elements size");
+        assertTrue(Collection2DTest.elements.containsAll(Collection2DTest.collection2D.toList()), "Collection2D toList does not contain all elements");
+        assertTrue(Collection2DTest.collection2D.toList().containsAll(Collection2DTest.elements), "Collection2D toList does not contain all elements");
+    }
+
+    @Test
+    void dimension() {
+        assertEquals(new Dimension(NB_COLUMNS, NB_ROWS), collection2D.getDimension(), "Collection2D dimension is not the same as the elements size");
+        Collection2DElementTest newElement = new Collection2DElementTest();
+        newElement.setPosition(new Point(1000, 2000));
+        Collection2DTest.collection2D.add(newElement);
+
+        assertEquals(new Dimension(1000, 2000), Collection2DTest.collection2D.getDimension(), "Collection2D dimension is not the same as the elements size");
+    }
+
+    @Test
+    void elementHasMoved() {
+        Collection2DElementTest elementTest = Collection2DTest.elements.get(0);
+
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> elementTest.setPosition(new Point(-100, -10)), "Element with negative position has been added");
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> elementTest.setPosition(new Point(100, -10)), "Element with negative position has been added");
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> elementTest.setPosition(new Point(-100, 10)), "Element with negative position has been added");
+    }
+
+    @Test
+    void isEmpty() {
+        assertFalse(Collection2DTest.collection2D.isEmpty(), "Collection2D is empty");
+        assertTrue(new Collection2D<Collection2DElementTest>().isEmpty(), "Collection2D is not empty");
+    }
+
+    @Getter
+    @Setter
+    private static class Collection2DElementTest implements ICollection2DElement<Collection2DElementTest> {
+        private Point position;
+        private Collection2D<Collection2DElementTest> collection;
+
+        public void moveTo(final Point newPosition) {
+            final Point oldPosition = this.position;
+            this.collection.notifyElementHasMoved(this, oldPosition);
+            this.position = newPosition;
+        }
+    }
+}
